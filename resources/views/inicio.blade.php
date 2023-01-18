20179dd184684f6cf67ade2c9f05e1de589e91fe
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2023-01-18 10:32:01 -0300

    CSS - Estilização da grade de visualização de membros

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -14,38 +14,35 @@
         <input type="submit" value="pesquisar">
     </form>
 
-    {{-- Como existem alunos cadastrados, então exiba-os com FOREACH --}}
-    <table class="w-1\1">
-        <tr id="table-title">
-            <th colspan="7" class="title-alunos" id="title-table-alunos">ADAD - Alunos
-                Cadastrados:</th>
-        </tr>
-        <tr class="w-1\1 line-header-alunos">
-            <th class="cabecalho-table">Nome</th>
-            <th class="cabecalho-table">Idade</th>
-            <th class="cabecalho-table">Série</th>
-            <th class="cabecalho-table">Cidade</th>
-            <th class="cabecalho-table">Religião</th>
-            <th class="cabecalho-table">Editar</th>
-            <th class="cabecalho-table" id="delete">Excluir</th>
-        </tr>
+    {{-- Exibir alunos cadastrados, utilizando foreach --}}
+    <div>Membros Cadastrados:</div>
+    <div id="exibir-membros">
+            {{-- nome deverá ter rowspan 2 ou 3 --}}
+            <button id="nome">Nome</button>
+            <button>Congregação</button>
+            <button>Nº Setor</button>
+            <button>Data de Nascimento</button>
+            <button>Situação</button>
+            <button>Naturalidade</button>
+            <button>Editar</button>
+            <button>Excluir</button>
         @foreach ($membros as $membro)
-            <tr class="w-1\1 line-body-alunos">
-                <td>{{ $membro->nome }}</td>
-                <td>{{ $membro->cidade }}</td>
-                <td>{{ $membro->celular }}</td>
-                <td class="btn-edit-del">
+            <div>
+                <a>{{ $membro->nome }}</a>
+                <a>{{ $membro->cidade }}</a>
+                <a>{{ $membro->celular }}</a>
+                <a>
                     <a href="{{ route('edit', ['id' => $membro->id]) }}">
                         &#128393;
                     </a>
-                </td>
-                <td class="btn-edit-del">
+                </a>
+                <a>
                     <form action="{{ route('destroy', ['id' => $membro->id]) }}" method="POST" id="btn-form-del">
                         @csrf
                         @method('DELETE')
                         <button>&#128465;</button>
                     </form>
-                </td>
-            </tr>
+                </a>
+            </div>
         @endforeach
-    </table>
+    </div>
--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -5,18 +5,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
-    {{-- Titulo da página--}}
+
+    {{-- Titulo da página --}}
     <title>@yield('titulo')</title>
 
     <link rel="stylesheet" type="text/css" href="/assets/css/styles.css">
 </head>
 
 <body>
-    @if (session('msg'))
-        <p>{{ session('msg') }}</p>
-    @endif
-    @yield('conteudo')
+    <aside>
+        <nav>
+            <a href="#">Secretaria</a>
+            <a href="#">Tesouraria</a>
+            <a href="#">Patrimonial</a>
+            <a href="#">Cadastro de Usuários</a>
+            <a href="#">Configrurações</a>
+        </nav>
+    </aside>
+
+    <main>
+        @if (session('msg'))
+            <p>{{ session('msg') }}</p>
+        @endif
+        @yield('conteudo')
+    </main>
+
 </body>
 
 </html>
